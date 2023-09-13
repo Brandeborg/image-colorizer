153f898ec9ef60e82eb64bcf8b246dde3ee860c9
Brandeborg <hallager2505@gmail.com>
2023-09-13 14:54:21 +0200

    Created loop to grayscale all target images

--- src/data_prep.py
+++ src/data_prep.py
@@ -3,7 +3,7 @@ import re
 import os
 sep = os.sep
 
-def to_grayscale(in_img_path: str, out_img_path: str):
+def to_grayscale(in_img_path: str, out_img_path: str) -> None:
     """Converts image at path `in_img_path` to grayscale an saves at path `out_img_path`. 
     If `out_img_path` does not exist, it is created.
 
@@ -22,11 +22,25 @@ def to_grayscale(in_img_path: str, out_img_path: str):
     img = Image.open(in_img_path).convert('L')
     img.save(out_img_path)
 
+def create_input_data(target_dir: str) -> None:
+    """Create input image files (grayscale) from target data (colored)
+
+    Args:
+        target_dir
+    """
+    for root, _dirs, files in os.walk(target_dir):
+        if files == []:
+            continue
+
+        for file in files:
+            in_path = sep.join([root, file])
+            out_path = re.sub("target", "input", in_path)
+            
+            to_grayscale(in_path, out_path)
+
 def main():
-    input_path = f".{sep}dataset{sep}input{sep}Chapter 1{sep}page 1.png"
-    output_path = re.sub("input", "target", input_path)
+    create_input_data(f"dataset{sep}target")
     
-    to_grayscale(input_path, output_path)
 
 if __name__ == "__main__":
     main()
