3856ad4c88ad12a0c707a3f239adaa7ffd5e9b04
Brandeborg <hallager2505@gmail.com>
2023-09-13 17:39:55 +0200

    Created conversion from grayscale to bw

--- src/data_prep.py
+++ src/data_prep.py
@@ -1,4 +1,4 @@
-from PIL import Image
+import cv2
 import re
 import os
 sep = os.sep
@@ -19,8 +19,33 @@ def to_grayscale(in_img_path: str, out_img_path: str) -> None:
         os.makedirs(dir_path)
 
     # convert and save
-    img = Image.open(in_img_path).convert('L')
-    img.save(out_img_path)
+    rgb_image = cv2.imread(in_img_path)
+    gray_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2GRAY)
+
+    cv2.imwrite(out_img_path, gray_image)
+
+    return gray_image
+
+def to_BW(grayscale, out_img_path: str):
+    """Converts MatLike grayscale image to black and white an saves at path `out_img_path`. 
+    If `out_img_path` does not exist, it is created.
+
+    Args:
+        graysacle: Path to an existing image file
+        out_img_path (str): Path where the black and white version of the input is saved
+    """
+    # extract dir parts of path to output file
+    dir_path = sep.join(out_img_path.split(sep)[:-1])
+    
+    # make dirs of they do not exist
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    # convert and save
+    _thresh, bw_image = cv2.threshold(grayscale, 127.5, 255, cv2.THRESH_BINARY)
+    cv2.imwrite(out_img_path, bw_image)
+
+    return bw_image
 
 def create_input_data(target_dir: str) -> None:
     """Create input image files (grayscale) from target data (colored)
@@ -34,9 +59,13 @@ def create_input_data(target_dir: str) -> None:
 
         for file in files:
             in_path = sep.join([root, file])
-            out_path = re.sub("target", "input", in_path)
+            out_path = re.sub("target", "input_grayscale", in_path)
             
-            to_grayscale(in_path, out_path)
+            grayscale = to_grayscale(in_path, out_path)
+
+            out_path = re.sub("target", "input_bw", in_path)
+            print(out_path)
+            to_BW(grayscale, out_path)
 
 def main():
     create_input_data(f"dataset{sep}target")
