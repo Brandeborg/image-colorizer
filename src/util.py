191a45a124819612d56c6a152fa7d06974018b82
Brandeborg <hallager2505@gmail.com>
2023-09-27 15:39:24 +0200

    Doc strings

--- src/util.py
+++ src/util.py
@@ -2,9 +2,20 @@ from from_scratch import ImageColorizerModel
 import torch
 
 def save_model(model: ImageColorizerModel, dir_path: str):
+    """Saves state_dict of ImageColorizerModel at dir_path
+
+    Args:
+        model (ImageColorizerModel): The model to save
+        dir_path (str): Where to save the model
+    """
     torch.save(model.state_dict(), dir_path)
 
 def load_model(dir_path: str):
+    """Loads an ImageColorizerModel
+
+    Args:
+        dir_path (str): Where the model is located
+    """
     model = ImageColorizerModel()
     model.load_state_dict(torch.load(dir_path))
     model.eval()
