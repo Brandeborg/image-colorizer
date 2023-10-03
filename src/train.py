3f7cdaa522bbac2ee7817ffe78889afd11a41cf0
Brandeborg <hallager2505@gmail.com>
2023-10-03 12:26:36 +0200

    Changed learning rate scheduler, such that it changes every training step, not just every epoch

--- src/train.py
+++ src/train.py
@@ -3,6 +3,8 @@ import eval
 import util
 import models
 
+from functools import partial
+
 import os
 from os import sep
 
@@ -16,6 +18,33 @@ load_dotenv()
 CNN_MODEL = os.getenv("CNN_MODEL")
 MODELS_DIR = os.getenv("MODELS_DIR")
 
+
+def _get_linear_schedule_with_warmup_lr_lambda(current_step: int, *, num_warmup_steps: int, num_training_steps: int) -> float:
+    """https://github.com/huggingface/transformers/blob/v4.33.3/src/transformers/optimization.py#L357
+
+    Starts low, and increases factor until reaching num_warmup_steps, where the factor will be 1, 
+    then it starts decreasing again.
+
+    Example:
+
+    num_warmup_steps = 100
+    num_training_steps = 1000
+    
+    current_step = 1
+    factor = 1 / 100 = 0.01
+
+    current_step = 100
+    factor = 1000-100 / 1000-100 = 1.00
+
+    current_step = 400
+    factor = 1000-400 / 1000-100 = 600 / 900 = 0.67
+
+    Returns:
+        float: Multiplicative factor for learning rate
+    """
+    if current_step < num_warmup_steps:
+        return float(current_step) / float(max(1, num_warmup_steps))
+    return max(0.0, float(num_training_steps - current_step) / float(max(1, num_training_steps - num_warmup_steps)))
 
 def training_loop(ModelType, save_dir, num_epochs=3):
     # set device
@@ -40,19 +69,30 @@ def training_loop(ModelType, save_dir, num_epochs=3):
     # https://pytorch.org/tutorials/beginner/pytorch_with_examples.html#pytorch-optim
     criterion = torch.nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-    scheduler = lr_scheduler.LinearLR(optimizer, start_factor=1, end_factor=0.1, total_iters=num_epochs)
 
-    # loops through batches, so input and target are really
-    # lists of inputs and targets
+    # calculate training_steps and choose 5% warmup steps
+    num_training_steps = num_epochs * len(train)
+    num_warmup_steps = int(num_training_steps * 0.05)
 
+    # assemble lambda function, which, given current_step during training, 
+    # will return a multiplicative factor for the learning rate
+    # (copied from HuggingFace source code)
+    lr_lambda = partial(_get_linear_schedule_with_warmup_lr_lambda, 
+                        num_warmup_steps=num_warmup_steps, 
+                        num_training_steps=num_training_steps)
+    lr_scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda)
+
+    
     print("Training")
     batch_num = 0
     for epoch in range(num_epochs):
+        # loops through batches, so input and target are really
+        # lists of inputs and targets
         for input, target in iter(train_dataloader):
             batch_num += 1
             print(f"Batch {batch_num} / {len(train_dataloader)}, Epoch {epoch+1} / {num_epochs}", end="\r")
             
-            if batch_num % 500 == 0:
+            if batch_num % 100 == 0:
                print()
                metrics = eval.eval_model(model, eval_dataloader, criterion, device)
                print(metrics)
@@ -65,15 +105,19 @@ def training_loop(ModelType, save_dir, num_epochs=3):
             loss = criterion(target_pred, target)
 
             # print loss
-            if batch_num % 100 == 0:
-                print({"training_loss": loss.item(), "learning_rate": scheduler.get_last_lr()[0]})
+            if batch_num % 50 == 0:
+                print({"training_loss": loss.item(), "learning_rate": lr_scheduler.get_last_lr()[0]})
 
             # Zero gradients, perform a backward pass, and update the weights.
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-        scheduler.step()
+            # change learning rate
+            # (I assume this simply increases "current_step" and calls the lr lambda)
+            lr_scheduler.step()
+
+        batch_num = 0
 
     util.save_model(model, save_dir)
 
